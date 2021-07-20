9f553f199cc85cc9ea9d7779cc6af614085eadf5
Damir Bolic <bolicd@gmail.com>
2021-07-20 21:16:06 +0200

    Added missing tests, small tweak for PersonRepository.GetPerson and connection string fix

--- EventStoreTests/Integration/EventStoreIntegrationTests.cs
+++ EventStoreTests/Integration/EventStoreIntegrationTests.cs
@@ -1,8 +1,11 @@
 ﻿using Core.Person;
+using Core.Person.Repositories;
 using EventStoreTests.Infrastructure;
+using FluentAssertions;
 using Infrastructure.Factories;
 using Infrastructure.Repositories;
 using NUnit.Framework;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EventStoreTests.Integration
@@ -12,11 +15,13 @@ namespace EventStoreTests.Integration
     {
 
         private IEventStore _eventStore;
+        private IPersonRepository _personRepository; 
 
         [SetUp]
         public void SetUp()
         {
             _eventStore = new EventStoreRepository(new SqlConnectionFactory(ConnectionString));
+            _personRepository = new PersonRepository(_eventStore);
         }
 
         [Test]
@@ -53,6 +58,32 @@ namespace EventStoreTests.Integration
             Assert.AreEqual(fetchedPerson.PersonAddress.Country, "country1");
             Assert.AreEqual(fetchedPerson.PersonAddress.ZipCode, "111222");
             Assert.AreEqual(fetchedPerson.PersonAddress.Street, "street1");
+        }
+
+        [Test]
+        public async Task Given_PersonAggregate_When_AddressChanged_Then_FetchWithPersonRepository()
+        {
+            var personId = new PersonId();
+
+            var personAggregate = Person.CreateNewPerson("Chuck", "Norris");
+            personAggregate.ChangePersonAddress("street1", "country1", "111222", "city");
+
+            await _eventStore.SaveAsync(personId, personAggregate.Version, personAggregate.DomainEvents, "PersonAggregate");
+
+            var results = await _personRepository.GetPerson(personId.ToString());
+            results.LastName.Should().Be("Norris");
+            results.FirstName.Should().Be("Chuck");
+        }
+
+        [Test]
+        public async Task Fetch_NonExistantpersonFromRepo_Should_Return_NUll()
+        {
+            var personId = new PersonId();
+
+            var results = await _personRepository.GetPerson(personId.ToString());
+
+            results.Should().BeNull();
+
         }
     }
 }
--- Infrastructure/Repositories/EventStoreRepository.cs
+++ Infrastructure/Repositories/EventStoreRepository.cs
@@ -5,7 +5,6 @@ using Infrastructure.Model;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
